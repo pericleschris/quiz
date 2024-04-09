9649fe0d7adb1f32fa1db771e7ed8be89f5a2332
Pericles Christian <pericleschrstn@gmail.com>
2024-04-09 10:36:25 -0300

    fix: fonts files

--- src/functions/font.ts
+++ src/functions/font.ts
@@ -1,29 +1,29 @@
 import localFont from "next/font/local";
 
 export const fontBold = localFont({
-  src: "../fonts/NotoSans-Regular.ttf",
+  src: "../fonts/gibson-bold.otf",
   weight: "700",
   variable: "--font-bold",
   display: "swap",
 });
 
 export const fontRegular = localFont({
-  src: "../fonts/NotoSans-SemiBold.ttf",
+  src: "../fonts/gibson-italic.otf",
   weight: "400",
   variable: "--font-regular",
   display: "swap",
 });
 
-// export const fontSemiBold = localFont({
-//   src: "./src/fonts/GibsonSemiBdIt.otf",
-//   weight: "600",
-//   variable: "--font-semibold",
-//   display: "swap",
-// });
+export const fontSemiBold = localFont({
+  src: "../fonts/gibson-semibold.otf",
+  weight: "600",
+  variable: "--font-semibold",
+  display: "swap",
+});
 
-// export const fontLight = localFont({
-//   src: "./src/fonts/GibsonLightIt.otf",
-//   weight: "300",
-//   variable: "--font-light",
-//   display: "swap",
-// });
+export const fontLight = localFont({
+  src: "../fonts/gibson-light.otf",
+  weight: "300",
+  variable: "--font-light",
+  display: "swap",
+});
