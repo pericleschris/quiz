7f2bf5fb78ac83f7fcab90894246071896bffcd9
Pericles Christian <pericleschrstn@gmail.com>
2024-04-09 10:24:03 -0300

    fix: update path fonts

--- src/functions/font.ts
+++ src/functions/font.ts
@@ -1,28 +1,28 @@
 import localFont from "next/font/local";
 
 export const fontBold = localFont({
-  src: "../fonts/GibsonBoldIt.otf",
+  src: "./src/fonts/GibsonBoldIt.otf",
   weight: "700",
   variable: "--font-bold",
   display: "swap",
 });
 
 export const fontRegular = localFont({
-  src: "../fonts/GibsonItalic.otf",
+  src: "./src/fonts/GibsonItalic.otf",
   weight: "400",
   variable: "--font-regular",
   display: "swap",
 });
 
 export const fontSemiBold = localFont({
-  src: "../fonts/GibsonSemiBdIt.otf",
+  src: "./src/fonts/GibsonSemiBdIt.otf",
   weight: "600",
   variable: "--font-semibold",
   display: "swap",
 });
 
 export const fontLight = localFont({
-  src: "../fonts/GibsonLightIt.otf",
+  src: "./src/fonts/GibsonLightIt.otf",
   weight: "300",
   variable: "--font-light",
   display: "swap",
